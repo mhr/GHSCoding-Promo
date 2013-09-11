c990fedd273dd980bf8f4fa0a2060e9dadd9c3d8
H1N1theI <kevinfll2008@gmail.com>
2013-09-10 20:31:47 -0400

    Added a mechanism to load all the images in a folder called images.

--- PromoProject/src/engine/PromoEngine.java
+++ PromoProject/src/engine/PromoEngine.java
@@ -1,19 +1,31 @@
 package engine;
 import com.golden.gamedev.Game;
 import java.awt.Graphics2D;
+import java.io.File;
+import java.util.Map;
+import java.awt.image.*;
 /**
  *
  * @author Kevin Wu
  */
 public class PromoEngine extends com.golden.gamedev.Game{
+    private Map<String, java.awt.image.BufferedImage> sprites;
     @Override
     public void initResources(){
-        //import all the sprites later.
+        File spritesFolder = new File("sprites");
+        File[] spritesFiles = spritesFolder.listFiles(); //trusts that all files are images.
+        for(int i = 0; spritesFiles[i++] != null; i++){
+            BufferedImage image = getImage(spritesFiles[i].getName());
+            sprites.put(image.getSource().toString().replace(".png", ""), image);
+        }
     }
     public void loadWorld(){
         
     }
     public void loadEntities(){
+        
+    }
+    public void getSprite(String id){
         
     }
     public void initPlayer(){
